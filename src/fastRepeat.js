72358e46aa6366722ac73622488f91e33d973f08
Robert Porter <rob@gradecam.com>
2015-02-18 10:28:23 -0800

    Removed unused debounce func.

--- src/fastRepeat.js
+++ src/fastRepeat.js
@@ -17,45 +17,6 @@ angular.module('gc.fastRepeat', []).directive('fastRepeat', ['$compile', '$parse
         else { return (new Date()).getTime(); }
     }
 
-    var debounce = function(func, wait, immediate) { // Underscore JS debounce function
-        // Underscore.js 1.7.0
-        // http://underscorejs.org
-        // (c) 2009-2014 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
-        // Underscore may be freely distributed under the MIT license
-
-        var timeout, args, context, timestamp, result;
-
-        var now = Date.now || function() { return new Date().getTime(); };
-
-        var later = function() {
-          var last = now() - timestamp;
-
-          if (last < wait && last > 0) {
-            timeout = setTimeout(later, wait - last);
-          } else {
-            timeout = null;
-            if (!immediate) {
-              result = func.apply(context, args);
-              if (!timeout) { context = args = null; }
-            }
-          }
-        };
-
-        return function() {
-          context = this;
-          args = arguments;
-          timestamp = now();
-          var callNow = immediate && !timeout;
-          if (!timeout) { timeout = setTimeout(later, wait); }
-          if (callNow) {
-            result = func.apply(context, args);
-            context = args = null;
-          }
-
-          return result;
-        };
-      };
-
     return {
         restrict: 'A',
         transclude: 'element',
